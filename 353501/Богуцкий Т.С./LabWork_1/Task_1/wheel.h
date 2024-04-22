7780dbb27b1cdf4b4b3d4e0fe6f2307b72c3b7b9
Tima B <timofeibogutski@gmail.com>
2024-04-23 00:53:49 +0300

    First Task

---
+++ 353501/Богуцкий Т.С./LabWork_1/Task_1/wheel.h
@@ -0,0 +1,20 @@
+#ifndef WHEEL_H
+#define WHEEL_H
+
+#include <QObject>
+#include <QGraphicsRectItem>
+#include <QTimer>
+
+
+
+class Wheel : public QObject, public QGraphicsRectItem
+{
+public:
+    QTimer *timer;
+    Wheel();
+    ~Wheel();
+    void Start();
+    void Restart();
+};
+
+#endif // WHEEL_H
